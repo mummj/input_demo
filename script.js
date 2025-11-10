99c51a234de7da8e4e6ec680a266b8d682bddb07
mummj <mummj@eou.edu>
2025-11-10 15:25:39 -0800

    inserted demo information for checkboxes and inputs

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+console.log('hello world');
+let button = document.getElementById('click');
+
+let click = button.onclick;
+function onclick(){
+if(click)
+    {document.write('click');
+}
+};
+onclick();
